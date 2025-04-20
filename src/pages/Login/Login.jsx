512471bb29742854646cc13405232196e80de03f
Wissem Jderi <asqr43641@gmail.com>
2025-04-20 14:48:40 +0100

    Refactor code structure for improved readability and maintainability

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,9 +2,41 @@ import React from "react";
 import "./Login.css";
 import assets from "../../assets/assets";
 const Login = () => {
+  
   return (
     <div className="login">
       <img src={assets.logo_big} alt="" className="logo" />
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          placeholder="username"
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email Adrdress"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button type="submit">Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>I Agree to the terms of use and privacy policy</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">
+            Already have an account? <span>Click here</span>{" "}
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
