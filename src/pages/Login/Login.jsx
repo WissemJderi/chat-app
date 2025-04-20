d9220fc101a295c60c6d5362105d3c3e69fa5902
Wissem Jderi <asqr43641@gmail.com>
2025-04-20 15:06:33 +0100

    feat: Implement dynamic state management for login/signup form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import assets from "../../assets/assets";
 const Login = () => {
-  
+  const [currentState, setCurrentState] = useState("Sign Up");
+  console.log(currentState);
   return (
     <div className="login">
       <img src={assets.logo_big} alt="" className="logo" />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="username"
-          className="form-input"
-          required
-        />
+        <h2>{currentState}</h2>
+        {currentState === "Sign Up" ? (
+          <input
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        ) : null}
         <input
           type="email"
           placeholder="Email Adrdress"
@@ -26,15 +29,37 @@ const Login = () => {
           className="form-input"
           required
         />
-        <button type="submit">Sign Up</button>
+        <button type="submit">
+          {currentState === "Sign Up" ? "Create account" : "Login now"}
+        </button>
         <div className="login-term">
           <input type="checkbox" />
           <p>I Agree to the terms of use and privacy policy</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">
-            Already have an account? <span>Click here</span>{" "}
-          </p>
+          {currentState === "Sign Up" ? (
+            <p className="login-toggle">
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  setCurrentState("Login");
+                }}
+              >
+                Login here
+              </span>{" "}
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Create an account{" "}
+              <span
+                onClick={() => {
+                  setCurrentState("Sign Up");
+                }}
+              >
+                Click here
+              </span>{" "}
+            </p>
+          )}
         </div>
       </form>
     </div>
