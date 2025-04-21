64222a2ccbf8a274f347a2e5c1499116f1e52b30
Wissem Jderi <asqr43641@gmail.com>
2025-04-21 19:55:43 +0100

    feat: Implement ProfileUpdate component with styling and form for user profile details

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-import "./ProfileUpdate";
+import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".png, .jpg, .jpej" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            Upload Profile Image
+          </label>
+          <input type="text" placeholder="Your Name" required />
+          <textarea placeholder="Write Your Profile Boi" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img src={assets.logo_icon} className="profile-pic" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
