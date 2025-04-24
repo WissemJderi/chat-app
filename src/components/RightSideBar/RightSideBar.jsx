19b798f11bb4d31ca3386c48b187866c65089082
Wissem Jderi <asqr43641@gmail.com>
2025-04-24 19:37:43 +0100

    feat: Implement authentication state handling and logout functionality in RightSideBar

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./RightSideBar.css";
 import assets from "../../assets/assets";
+import { logout } from "../../config/firebase";
 const RightSideBar = () => {
   return (
     <div className="right-s">
@@ -23,7 +24,7 @@ const RightSideBar = () => {
           <img src={assets.pic3} alt="" />
         </div>
       </div>
-      <button>Logout</button>
+      <button onClick={() => logout()}>Logout</button>
     </div>
   );
 };
